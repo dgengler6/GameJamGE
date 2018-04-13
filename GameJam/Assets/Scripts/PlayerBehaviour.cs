887fcd72cfcaa40de4412bb8a65daad23b63298e
Gengler Damien <gengler.damien@gmail.com>
2018-04-13 14:21:28 +0200

    Added scene

---
+++ GameJam/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public Rigidbody2D myRigidBody;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+    	myRigidBody.AddForce (movement * speed);
+    }
+}
