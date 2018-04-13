a53063c2479dbfc3553c4037a0d4b1a1751f03b1
Gengler Damien <gengler.damien@gmail.com>
2018-04-13 17:45:47 +0200

    Finished player

--- GameJam/Assets/Scripts/PlayerBehaviour.cs
+++ GameJam/Assets/Scripts/PlayerBehaviour.cs
@@ -4,22 +4,72 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-	public Rigidbody2D myRigidBody;
+	public Rigidbody2D rb;
+	public BoxCollider2D boxC;
+
+	public Sprite sprite;
 	public float speed;
+	private bool isOnGround;
+
+	public bool isMovingSide;
 
 	// Use this for initialization
 	void Start () {
-		myRigidBody = GetComponent<Rigidbody2D>();
-		
+		Setup(new Vector2(0,10));
+	}
+
+	void Setup(Vector2 spawn_position){
+		rb = this.GetComponent<Rigidbody2D>();
+		boxC = this.GetComponent<BoxCollider2D>();
+		sprite = this.GetComponent<SpriteRenderer>().sprite;
+		isOnGround = false;
+		rb.position = spawn_position;
 	}
 	
-	void FixedUpdate ()
+	void Update ()
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-    	myRigidBody.AddForce (movement * speed);
+		if(!isOnGround){
+        	rb.velocity += new Vector2(0f, -9.81f)*Time.deltaTime;
+		}
+		KeyPressed();
     }
+
+	void KeyPressed(){
+
+		if(Input.GetButtonDown("Fire2")){
+			print("jump");
+			if(isOnGround){
+				rb.velocity += new Vector2(0f, 7f);
+				isOnGround = false;
+			}
+		}
+
+		if(Input.GetButtonDown("Fire1")){
+			Die();
+		}
+
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		rb.velocity += new Vector2(horizontal*speed*Time.deltaTime,0f);
+		if(horizontal == 0 && isOnGround){
+			rb.velocity = new Vector2(0f, rb.velocity.y);
+		}
+	}
+	void OnTriggerEnter2D(Collider2D collision){
+		print("Collision");
+		if(!isOnGround){
+			rb.velocity-= new Vector2(0f,rb.velocity.y);
+			isOnGround = true;
+		}
+	}
+
+
+	void Die(){
+		print("YOU LOST");
+		Reset();
+		
+	}
+
+	void Reset(){
+		Application.LoadLevel("Game");
+	}
 }
